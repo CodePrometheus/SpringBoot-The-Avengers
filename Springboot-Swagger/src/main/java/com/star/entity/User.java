a3190079e7b8404eb91564e7da248b6121b10175
CodePrometheus <Code_Prince@outlook.com>
2020-08-24 15:05:01 +0800

    Springboot-Swagger

---
+++ Springboot-Swagger/src/main/java/com/star/entity/User.java
@@ -0,0 +1,39 @@
+package com.star.entity;
+
+
+public class User {
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    public User() {
+    }
+
+    public User(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String name;
+    private String id;
+}
