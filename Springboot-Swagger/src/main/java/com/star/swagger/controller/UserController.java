1e21326792b7c453a25258bc93fc6f5bf8d06a58
CodePrometheus <Code_Prince@outlook.com>
2021-04-11 20:09:20 +0800

    Update

--- Springboot-Swagger/src/main/java/com/star/swagger/controller/UserController.java
+++ Springboot-Swagger/src/main/java/com/star/swagger/controller/UserController.java
@@ -5,11 +5,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
